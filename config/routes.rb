0a898a19f0bd976abebb33372898a531fbc6b99d
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-10-19 23:23:33 -0500

    Created an App for HW assignment with scaffold gen, controller gen, devise gem, and custom routes for controller pages

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
+
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+end
